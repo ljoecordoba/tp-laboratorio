565b249302388fa39371b09e5e45133517b4c8e6
luciano <ljoecordoba@gmail.com>
2018-04-17 03:26:19 -0300

    reserva de salas

---
+++ src/main/java/domain/Materia.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Materia
+{
+	int id;
+	String nombre;
+	public Materia(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+}
---
+++ src/main/java/modelo/ReservaSalas.java
@@ -0,0 +1,23 @@
+package modelo;
+
+import java.util.List;
+
+import domain.ReservaSala;
+import domain.Sala;
+
+public class ReservaSalas
+{
+	private List<Sala> salas;
+	private List<ReservaSala> reservas;
+	public ReservaSalas()
+	{
+		//Cargar salas y reservas de configuración
+	}
+	
+	
+	public void reservarSala(ReservaSala reserva)
+	{
+		
+	}
+	
+}
